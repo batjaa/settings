1ad528352aec7ee74d448168d2c074fd52899a79
batjaa <batjaa0615@gmail.com>
2015-07-16 22:00:42 -0700

    Add android sdk to brew

--- brew.sh
+++ brew.sh
@@ -51,6 +51,7 @@ brew install dark-mode
 #brew install exiv2
 brew install git
 brew install git-lfs
+brew install android-sdk
 # brew install imagemagick --with-webp
 # brew install lua
 # brew install lynx
