693d83a21342996d3718996d53b3ac84c2779aff
Batjaa Batbold <batjaa0615@gmail.com>
2018-12-06 17:00:00 -0800

    Update app list

--- brew-cask.sh
+++ brew-cask.sh
@@ -10,25 +10,34 @@
 brew install caskroom/cask/brew-cask
 brew tap caskroom/versions
 
-# daily
 brew cask install slate
-brew cask install karabiner
+brew cask install bettertouchtool
+brew cask install alfred
+brew cask install karabiner-elements
+
+# daily
 brew cask install dropbox
 brew cask install slack
-brew cask install google-drive
-# brew cask install gyazo
+brew cask install google-drive-file-stream
 brew cask install rescuetime
-brew cask install alfred
-# brew cask install flux
+brew cask install vlc
+brew cask install spotify
 
 # dev
 brew cask install iterm2
-brew cask install p4merge
-brew cask install sublime-text3
+brew cask install sublime-text
+brew cask install sublime-merge
 brew cask install dash
 brew cask install postman
+brew cask install trailer
+brew install hub
 # brew cask install imagealpha
 # brew cask install imageoptim
+
+# quartzy
+brew cask install sequel-pro
+brew install composer
+brew cask install jetbrains-toolbox
 
 # fun
 # brew cask install limechat
@@ -39,14 +48,15 @@ brew cask install postman
 brew cask install google-chrome
 brew cask install google-chrome-canary
 brew cask install firefox-nightly
-brew cask install webkit-nightly
 brew cask install chromium
-brew cask install torbrowser
+brew cask install tor-browser
+
+# video and photo editing
+brew cask install adobe-creative-cloud
 
 # less often
+brew cask install viber
+# brew cask install daisydisk # install using app store
+# brew cask install balenaetcher
 # brew cask install disk-inventory-x
 # brew cask install screenflow4 # 4 specifically not 5.
-brew cask install vlc
-brew cask install gpgtools
-# brew cask install licecap
-brew cask install utorrent
