4626ac574aad0eeb68151de6188a2cf22300f3cf
Batjaa Batbold <batjaa0615@gmail.com>
2020-06-03 15:36:17 -0700

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -14,30 +14,40 @@ brew update
 # Upgrade any already-installed formulae.
 brew upgrade --all
 
-# Install GNU core utilities (those that come with OS X are outdated).
+# Save Homebrew’s installed location.
+BREW_PREFIX=$(brew --prefix)
+
+# Install GNU core utilities (those that come with macOS are outdated).
 # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
-#brew install coreutils
-#sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
+brew install coreutils
+ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
 
 # Install some other useful utilities like `sponge`.
-#brew install moreutils
+brew install moreutils
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
 #brew install findutils
 # Install GNU `sed`, overwriting the built-in `sed`.
 #brew install gnu-sed --with-default-names
-# Install Bash 4.
-# Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
-# running `chsh`.
-# brew install bash
-# brew tap homebrew/versions
-# brew install bash-completion2
+# Install a modern version of Bash.
+brew install bash
+brew install bash-completion2
 
-# Install more recent versions of some OS X tools.
-brew install vim --override-system-vi
-brew install homebrew/dupes/grep
-brew install homebrew/dupes/openssh
-brew install homebrew/dupes/screen
-# brew install homebrew/php/php55 --with-gmp
+# Switch to using brew-installed bash as default shell
+if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
+  echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
+  chsh -s "${BREW_PREFIX}/bin/bash";
+fi;
+
+# Install `wget` with IRI support.
+brew install wget --with-iri
+
+# Install more recent versions of some macOS tools.
+brew install vim --with-override-system-vi
+brew install grep
+brew install openssh
+brew install screen
+brew install php
+brew install gmp
 
 # Install font tools.
 brew tap bramstein/webfonttools
@@ -47,30 +57,21 @@ brew install woff2
 
 # Install other useful binaries.
 brew install ack
-brew install dark-mode
-brew install moreutils
-#brew install exiv2
-brew install docker
-brew install git
-brew install hub
+brew install brightness
 brew install composer
-brew install nvm
+brew install git
 brew install git-lfs
-brew install android-sdk
-# brew install imagemagick --with-webp
-# brew install lua
-# brew install lynx
+brew install gh
+brew install imagemagick --with-webp
+brew install lua
+brew install nvm
 # brew install p7zip
 # brew install pigz
 # brew install pv
 brew install rename
-# brew install rhino
-# brew install speedtest_cli
-# brew install ssh-copy-id
-# brew install tree
-# brew install webkit2png
+brew install tree
+brew install vbindiff
 # brew install zopfli
-brew install brightness
 
 # Remove outdated versions from the cellar.
 brew cleanup
