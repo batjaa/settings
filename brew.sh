f50c634f87589d33c266ac06e7305b9fc4d7a313
Batjaa Batbold <batjargal.batbold@samsara.com>
2022-07-20 08:52:02 -0700

    Update gitignore

--- brew.sh
+++ brew.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-# Install command-line tools using Homebrew.
+# Install command-line tools using Homebrew
 
-# Make sure we’re using the latest Homebrew.
+# Make sure we’re using the latest Homebrew
 brew update
 
-# Upgrade any already-installed formulae.
+# Upgrade any already-installed formulae
 brew upgrade
 
-# Save Homebrew’s installed location.
+# Save Homebrew’s installed location
 BREW_PREFIX=$(brew --prefix)
 
 # Install GNU core utilities (those that come with macOS are outdated).
@@ -19,9 +19,9 @@ ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
 # Install some other useful utilities like `sponge`.
 brew install moreutils
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
-#brew install findutils
+# brew install findutils
 # Install GNU `sed`, overwriting the built-in `sed`.
-#brew install gnu-sed --with-default-names
+# brew install gnu-sed --with-default-names
 # Install a modern version of Bash.
 brew install bash
 brew install bash-completion2
@@ -37,10 +37,9 @@ brew install wget
 # Install more recent versions of some macOS tools.
 brew install vim --with-override-system-vi
 brew install grep
-brew install openssh
+# brew install openssh
 brew install screen
-brew install php
-brew install gmp
+# brew install php
 
 # Install font tools.
 brew tap bramstein/webfonttools
@@ -51,7 +50,7 @@ brew install woff2
 # Install other useful binaries.
 brew install ack
 brew install brightness
-brew install composer
+# brew install composer
 brew install git
 brew install git-lfs
 brew install gh
@@ -59,7 +58,7 @@ brew install jq
 brew install imagemagick --with-webp
 brew install lua
 brew install nvm
-brew install yarn
+# brew install yarn
 brew install asdf
 # brew install p7zip
 # brew install pigz
@@ -72,3 +71,68 @@ brew install blueutil
 
 # Remove outdated versions from the cellar.
 brew cleanup
+
+# to maintain cask ....
+#     brew update && brew upgrade brew-cask && brew cleanup && brew cask cleanup`
+
+
+# Install native apps
+
+# daily
+brew install slate
+brew install bettertouchtool
+# brew install karabiner
+# brew install dropbox
+brew install slack
+brew install rescuetime
+brew install alfred
+brew install spotify
+brew install google-backup-and-sync
+# brew install discord
+brew install 1password
+
+# dev
+brew install iterm2
+brew install p4v
+brew install sublime-text
+brew install visual-studio-code
+brew install dash
+# brew install postman
+# brew install jetbrains-toolbox
+brew install trailer
+# brew install sequel-pro
+
+# communication
+# brew install whatsapp
+# brew install viber
+# brew install telegram
+
+# creativity
+# brew install adobe-creative-cloud
+
+# brew install imagealpha
+# brew install imageoptim
+
+# fun
+# brew install limechat
+# brew install miro-video-converter
+# brew install horndis               # usb tethering
+# brew install steam
+
+# browsers
+# brew install google-chrome
+# brew install google-chrome-canary
+# brew install homebrew/cask-versions/firefox-developer-edition
+# brew install webkit-nightly
+# brew install chromium
+# brew install tor-browser
+
+# less often
+# brew install disk-inventory-x
+# brew install screenflow4 # 4 specifically not 5.
+# brew install vlc
+# brew install gpgtools
+# brew install licecap
+# brew install utorrent
+# brew install garmin-express
+brew install daisydisk
